efef18ea2c0364497107da7a5e529a41e9432c61
BethMwangi <wanjikumwangib@gmail.com>
2016-08-18 18:46:54 +0300

    [super_sum.py] implemented code to test the cases
    implementing the super sum of all arguments passed

--- TDD/super_sum/super_sum.py
+++ TDD/super_sum/super_sum.py
@@ -5,7 +5,26 @@ def super_sum(*args):
 	total  sum
 	'''
 
-	pass
+	total = 0 #initialize the total for elements in a list
+	int_num = 0 #adds the integers passed 
+
+	for element in args: # loops through each element passed
+		if type(element) is list: #test if the element is a list
+			for i in element:
+				if type(i) is int:
+					total = i + total # adds the elements in an individual list
+				else:
+					return ' You have entered a non integer'
+		else:
+			if type(element) is int: #test if the element passed is an integer
+				int_num = element + int_num
+			else:
+				return 'you entered a non integer item'
+
+	super_total = int_num + total #adds the super sum of all elements
+	return super_total
+
+	
 
 	
 
