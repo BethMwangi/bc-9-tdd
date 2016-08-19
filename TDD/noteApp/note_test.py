139406921aca7e85f0221fd45edfa139fea6fc43
BethMwangi <wanjikumwangib@gmail.com>
2016-08-19 16:28:55 +0300

    [note_test.py] test to check for empty note

--- TDD/noteApp/note_test.py
+++ TDD/noteApp/note_test.py
@@ -4,6 +4,7 @@ from note import NotesApplication
 class TestNoteApplication(unittest.TestCase):
 
 	def test_int_type_notecontent(self):
+
 		''' pass test for integer inputs'''
 		author = NotesApplication('author')
 		author.create('note_content')
@@ -11,10 +12,19 @@ class TestNoteApplication(unittest.TestCase):
 		
 
 	def test_string_type_notecontent(self):
+
 		''' pass test for string inputs'''
 		author = NotesApplication('author')
 		author.create('note_content')
 		self.assertEqual(type(val), str)
+
+	def test_note_is_empty(self):
+
+		''' test if the note is empty '''
+		author1 = NotesApplication('author one')
+		author1.create()
+		self.assertIsEqual(author1.list(), 'None')
+
 		
 
 
