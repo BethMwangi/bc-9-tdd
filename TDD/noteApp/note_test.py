58e3cb8d54207b62fb2134da8ca9830368479389
BethMwangi <wanjikumwangib@gmail.com>
2016-08-19 16:09:03 +0300

    [note_test.py] seperate two test cases
    seperate test case for integer and string

--- TDD/noteApp/note_test.py
+++ TDD/noteApp/note_test.py
@@ -3,12 +3,21 @@ from note import NotesApplication
 
 class TestNoteApplication(unittest.TestCase):
 
-	def test_int_and_string_type_notecontent(self):
-		''' pass test for string and integer inputs'''
+	def test_int_type_notecontent(self):
+		''' pass test for integer inputs'''
+		author = NotesApplication('author')
+		author.create('note_content')
+		self.assertEqual(type(val), int)
+		
+
+	def test_string_type_notecontent(self):
+		''' pass test for string inputs'''
 		author = NotesApplication('author')
 		author.create('note_content')
 		self.assertEqual(type(val), str)
-		self.assertEqual(type(val), int)
+		
+
+
 
 
 
